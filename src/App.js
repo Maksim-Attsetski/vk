1f45a05cb45ff17df833a60229709649996e6a5a
Maksim-Attsetski <maks2001maks2001@gmail.com>
2022-06-02 20:11:44 +0300

    Square

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <div>
+          <div className="square" style={{width: 100, height: 100, background: 'green'}}></div>
+        </div>
+    );
+};
 
 export default App;
