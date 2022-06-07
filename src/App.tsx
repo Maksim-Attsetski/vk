0bf31c77489d4b4c8398d97a11896fd57523d081
Maksim-Attsetski <maks2001maks2001@gmail.com>
2022-06-07 14:51:47 +0300

    Init

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const App = () => {
     return (
         <div>
-          <div className="square" style={{width: 100, height: 100, background: 'green'}}></div>
+          worked
         </div>
     );
 };
